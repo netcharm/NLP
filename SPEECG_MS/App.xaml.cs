3afcd0c738c8aaf9f41ace167cd9c5c4d4e59720
netcharm <netcharm@163.com>
2022-01-27 15:46:07 +0800

    Add: Init tet project of speech using azure services

--- SPEECG_MS/App.xaml.cs
+++ SPEECG_MS/App.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
@@ -13,5 +14,16 @@ namespace SPEECH_MS
     /// </summary>
     public partial class App : Application
     {
+        public Common.Setting Setting { get; set; } = new Common.Setting();
+
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            var config_file = Path.Combine(Common.Setting.APP_ROOT, Path.ChangeExtension(Common.Setting.APP_NAME, ".json"));
+            if (File.Exists(config_file))
+            {
+                var config = File.ReadAllText(config_file);
+                Setting = Newtonsoft.Json.JsonConvert.DeserializeObject<Common.Setting>(config);
+            }
+        }
     }
 }
---
+++ SPEECG_MS/Common/Setting.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SPEECH_MS.Common
+{
+    public class API
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string Providor { get; set; } = string.Empty;
+        public string Category { get; set; } = string.Empty;
+        public string Domain { get; set; } = string.Empty;
+        public string Token { get; set; } = string.Empty;
+        public string User { get; set; } = string.Empty;
+        public string Pass { get; set; } = string.Empty;
+        public string Url { get; set; } = string.Empty;
+        public string Key { get; set; } = string.Empty;
+        public string Security { get; set; } = string.Empty;
+    }
+
+    public class Setting
+    {
+        // System.Environment.GetCommandLineArgs()[0];
+        // System.AppDomain.CurrentDomain.BaseDirectory
+        // System.IO.Path.GetDirectoryName(System.Diagnostics.Process.GetCurrentProcess().MainModule.FileName)
+        // System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+        // System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().GetModules()[0].FullyQualifiedName)
+        static public string APP_ROOT { get; } = AppDomain.CurrentDomain.BaseDirectory;
+        static public string APP_NAME { get; } = AppDomain.CurrentDomain.FriendlyName;
+
+        public List<API> API_List { get; set; } = new List<API>();
+    }
+}
